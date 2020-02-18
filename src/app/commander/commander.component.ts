ff6ff09791bca69b6c5d7676fec62738ef37fba5
Vuillaume.Adrien <adrien.vuillaume@etu.univ-cotedazur.fr>
2020-02-18 16:19:25 +0100

    Working version of commander without panier.

--- src/app/commander/commander.component.ts
+++ src/app/commander/commander.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-commander',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commander.component.css']
 })
 export class CommanderComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(public fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      firstname: [''],
+      lastname: [''],
+      address: [''],
+      postalcode: [''],
+      city: ['']
+    })
+   }
 
   ngOnInit(): void {
   }
+
+  submitForm() {
+    var formData: any = new FormData();
+    formData.append("firstname", this.form.get('firstname').value);
+    formData.append("lastname", this.form.get('lastname').value);
+    formData.append("address", this.form.get('address').value);
+    formData.append("postalcode", this.form.get('postalcode').value);
+    formData.append("city", this.form.get('city').value);
+
+    this.http.post('http://dam.tokidev.fr/customers', formData).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error),
+    );
+  }
+
+
 
 }
