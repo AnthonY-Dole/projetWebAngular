263912b2b35e9ec1612e8d4609280db2fa4c765b
Dole.anthony <anthony.dole@etu.unice.fr>
2020-02-18 14:17:34 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoutiqueComponent } from './boutique/boutique.component';
+import { IndexComponent } from './index/index.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [ { path: 'boutique', component: BoutiqueComponent },
+{ path: 'index', component: IndexComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoutiqueComponent } from './boutique/boutique.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoutiqueComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
